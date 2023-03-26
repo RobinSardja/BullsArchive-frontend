149afb908c92544e8857be42e9d5e672d6f17b91
Robin Sardja <robinsardja@gmail.com>
2023-03-26 02:14:05 -0400

    Search bar font change

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = ({ showCourses, handleShowCoursesChange }) => {
   return (
     <div className='py-20 flex flex-col gap-8 justify-center items-center'>
-      <h2 className='text-slate-100 font-extrabold text-3xl tracking-widest'>
+      <h2 className='text-slate-100 font-bold text-3xl tracking-widest'>
         SEARCH A COURSE
       </h2>
       <input className='p-4 outline-none h-12 w-[400px] sm:w-[500px] md:w-[650px] lg:w-[800px] rounded-3xl bg-zinc-400 focus:bg-green-50/90 transition-all duration-150' type="text" value={showCourses} onChange={handleShowCoursesChange} />
